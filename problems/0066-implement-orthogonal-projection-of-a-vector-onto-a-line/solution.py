e313cf5501cc396385f069bbb3f183c4eb68f924
dai9u9r3jTienOofwm <177824409+dai9u9r3jTienOofwm@users.noreply.github.com>
2026-09-20 09:28:28 +0000

    Solve: Implement Orthogonal Projection of a Vector onto a Line

---
+++ problems/0066-implement-orthogonal-projection-of-a-vector-onto-a-line/solution.py
@@ -0,0 +1,15 @@
+
+import numpy
+
+def orthogonal_projection(v, L):
+	"""
+	Compute the orthogonal projection of vector v onto line L.
+
+	:param v: The vector to be projected
+	:param L: The line vector defining the direction of projection
+	:return: List representing the projection of v onto L
+	"""
+	v = numpy.array(v)
+	L = numpy.array(L)
+	return ((numpy.dot(v,L)/numpy.dot(L,L)) * L).tolist()
+	
